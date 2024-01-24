064ab2e76c40d73f0b7f91df88a7153d99bd7c1c
Eugeny1978 <luchnik78@mail.ru>
2024-01-24 19:33:37 +0300

    Modify Files

--- AbstractFactory.py
+++ AbstractFactory.py
@@ -49,30 +49,35 @@ class IFactory(metaclass=ABCMeta):
 
 class JapaneseFactory(IFactory):
     def create_engine(self) -> IEngine:
-        return JapaneseEngine
+        return JapaneseEngine()
     def create_car(self) -> ICar:
-        return JapaneseCar
+        return JapaneseCar()
 
 class RussianFactory(IFactory):
     def create_engine(self) -> IEngine:
-        return RussianEngine
+        return RussianEngine()
     def create_car(self) -> ICar:
-        return RussianCar
+        return RussianCar()
 
 class СhineseFactory(IFactory):
     def create_engine(self) -> IEngine:
-        return ChineseEngine
+        return ChineseEngine()
     def create_car(self) -> ICar:
-        return СhineseCar
-
+        return СhineseCar()
 
 
 if __name__ == '__main__':
+    jp_engine = JapaneseEngine()
+    jp_car = JapaneseCar()
+    print('jp_engine', jp_engine)
+    print('jp_car', jp_car)
+
     j_factory = JapaneseFactory()
     j_engine = j_factory.create_engine()
     j_car = j_factory.create_car()
     print('j_engine', j_engine)
     print('j_car', j_car)
+
 
     r_factory = RussianFactory()
     r_engine = r_factory.create_engine()
@@ -82,9 +87,11 @@ if __name__ == '__main__':
     ch_engine = ch_factory.create_engine()
     ch_car = ch_factory.create_car()
 
-    j_car.release_car(j_engine)
-    # r_car.release_car(r_engine)
-    # ch_car.release_car(ch_engine)
+    j_car.release_car(engine=j_engine)
+    r_car.release_car(r_engine)
+    ch_car.release_car(ch_engine)
+
+
 
 
 
